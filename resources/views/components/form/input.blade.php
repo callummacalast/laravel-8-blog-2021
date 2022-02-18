14195b1778c3cfd3256c8ccb859923618705d18a
Callum Macalast <callummacalast@Callums-MacBook-Pro-2.local>
2022-02-18 23:23:01 +0000

    Changes for edit route

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -2,7 +2,7 @@
 
 <div class="mb-6">
     <x-form.label name="{{ $name }}" />
-    <input type="{{$type}}" class="border border-gray-200 p-2 w-full rounded " type="text" name="{{$name}}" id="{{$name}}" required {{ $attributes([ 'value' => old($name)])}}>
+    <input type="{{$type}}" class="border border-gray-200 p-2 w-full rounded " type="text" name="{{$name}}" id="{{$name}}" {{ $attributes([ 'value' => old($name)])}}>
 
     <x-form.error name="{{ $name }}" />
 </div>
